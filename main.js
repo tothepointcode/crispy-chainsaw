1191a496e08d7dcbabb22f5fbe95bafd56250c74
cobytab <terra.andoh@gmail.com>
2021-07-15 18:14:04 +0000

    Path requirement added to main.js file.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // This is the main starting point for the electron application
 
 const { app, BrowserWindow } = require("electron");
+const path = require('path');
 
 function createWindow() {
   // web preferences key added to load preload script
