e1ad11132d282ca64eeb32c1277ed227f30c16b4
cobytab <terra.andoh@gmail.com>
2021-07-19 23:38:50 +0000

    feat: :construction: Each key now outputs to the input field
    Manipulated renderer.js to output each button's value to the input field for now.

--- renderer.js
+++ renderer.js
@@ -8,3 +8,19 @@
 
 // new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY }).onclick =
 //   () => (document.getElementById("output").innerText = CLICK_MESSAGE);
+
+// Targetting input field
+let fieldValue = ``;
+
+const field = document.querySelector("input#field");
+field.value = fieldValue;
+
+let buttons = document.querySelectorAll(".keys button");
+
+buttons.forEach((button) =>
+  button.addEventListener("click", (event) => {
+    let buttonValue = event.target.textContent;
+    // fieldValue = `${fieldValue} ${buttonValue}`;
+    field.value = buttonValue;
+  })
+);
